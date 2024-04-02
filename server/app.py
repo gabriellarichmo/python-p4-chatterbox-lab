d2c1b314ca671c1680049bb0c5d4cb266cbe0597
Gabriella Richmond <gabriella.m.richmond@gmail.com>
2024-04-02 15:21:48 -0700

    complete chatterbox lab

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
+from flask_restful import Api, Resource
 
 from models import db, Message
 
@@ -11,16 +12,81 @@ app.json.compact = False
 
 CORS(app)
 migrate = Migrate(app, db)
-
+api = Api(app)
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+# @app.route('/messages')
+# def messages():
+#     return ''
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+# @app.route('/messages/<int:id>')
+# def messages_by_id(id):
+#     return ''
+
+class Messages(Resource):
+    def get(self):
+        try:
+            serialized_messages = [message.to_dict() for message in Message.query.order_by(Message.created_at.asc())]
+            return serialized_messages, 200
+        except Exception as e:
+            return str(e), 400
+
+    def post(self):
+        try:
+            data = request.get_json()
+            new_message = Message(**data)
+
+            db.session.add(new_message)
+            db.session.commit()
+            
+            message_dict = new_message.to_dict()
+
+            return message_dict, 201
+        
+        except Exception as e:
+            return str(e), 400
+        
+api.add_resource(Messages, '/messages')
+
+class MessageByID(Resource):
+    def get(self, id):
+        try:
+            message = db.session.get(Message, id).to_dict()
+
+            # message_dict = message.to_dict()
+
+            return message, 200
+        except Exception as e:
+            return str(e), 400
+
+    def patch(self, id):
+        try:
+            if message := db.session.get(Message, id):
+                for attr in request.get_json():
+                    setattr(message, attr, request.get_json()[attr])
+
+                db.session.commit()
+
+                message_dict = message.to_dict()
+
+                return message_dict, 200
+            else:
+                return {"message": f"Could not find {id}"}
+            
+        except Exception as e:
+            return str(e), 400
+        
+    def delete(self, id):
+        try:
+            message = db.session.get(Message, id)
+
+            db.session.delete(message)
+            db.session.commit()
+            return {}, 204
+        except Exception as e:
+            return str(e), 400
+
+api.add_resource(MessageByID, '/messages/<int:id>')
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5555, debug=True)
